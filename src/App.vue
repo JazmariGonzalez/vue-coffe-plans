a57ea4ea6f2860c4310851bd3b983742194a9acd
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-04-15 17:51:31 -0600

    🚧 First Propieties

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import planItem from './components/icons/plan-item.vue';
+import planItem from './components/plan-item.vue'
 </script>
-
+ 
 <template>
   <header>
     <div>
@@ -9,44 +9,44 @@ import planItem from './components/icons/plan-item.vue';
       <img src="./assets/logo.png" alt="logo">
     </div>
   </header>
-
+ 
   <div class="content">
     <h1 class="title">Coffee Plans</h1>
-
+ 
     <h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
-
+ 
     <div class="plans">
-      <plan-item/>
-      <plan-item/>
-      <plan-item/>
-      <plan-item/>
-          </div>
+      <plan-item name="El soltero" />
+      <plan-item name="El viajero" />
+      <plan-item name="El adulto" />
+      <plan-item />
+    </div>
   </div>
 </template>
-
+ 
 <style scoped>
 header {
   line-height: 1.5;
 }
-
+ 
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-
+ 
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
+ 
   .logo {
     margin: 0 2rem 0 0;
   }
-
+ 
   header .wrapper {
     display: flex;
     place-items: flex-start;
---
+++ src/components/plan-item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="plan">
+         <div class="description">
+           <span class="title">  
+             {{ name }}
+           </span>
+         </div>
+       </div>
+  </template>
+  
+  <script setup>
+  //Macro es una funcion especialidad en vue que me permite crear defineProps (que ya las importa), no se tiene que intregrar porque Vue ya lo hace, para agregar  nombre propiedades siempre usar camelcassing
+  defineProps(['name']);
+  
+  </script>
