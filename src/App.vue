b79f9db1a41bbca5f253f74c922b4c488d08bcd9
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-05-02 19:55:40 -0600

    🎉FIN Slots

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,22 @@ import FancyButton from './components/FancyButton.vue'
 </script>
  
 <template>
-<FancyButton/>
+<FancyButton> 
+    <template v-slot:icon ="slotprops"> 
+         {{ slotprops.hover ? '🚀': '❌'}} 
+    </template> 
+    <template v-slot:default> 
+    <strong style="text-decoration: underline;">E</strong>nviar 
+</template> 
+</FancyButton>
+<FancyButton> 
+    <template v-slot:icon = "{ hover }"> 
+     {{ hover  ? '❌': '🚀'}} 
+    </template> 
+    <template v-slot:default> 
+    <strong style="text-decoration: underline;">C</strong>ancelar
+</template> 
+</FancyButton>
 </template>
  
 <style scoped>
--- src/components/FancyButton.vue
+++ src/components/FancyButton.vue
@@ -1,11 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+const hover = ref(false)
 </script>
+
 <template>
     <div class="content">
-      <button> Haz Clic 😉</button>
+      <button 
+      @mouseenter="hover=true"
+      @mouseleave="hover=false">
+        <slot name ="icon" :hover ="hover"> ⚠ </slot>
+        <slot> No Message </slot>
+      </button>
+      {{ hover }} 
     </div>
   </template>
-   
   <style scoped>
   .content{
     text-align: center;
